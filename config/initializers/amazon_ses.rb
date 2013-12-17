f8cacdfeec3168a0e3dd1d8c04096f1169fe40f2
Amit Sharma <amitkumarsharma@gmail.com>
2013-12-17 12:14:58 +0530

    amazon ses settings

---
+++ config/initializers/amazon_ses.rb
@@ -0,0 +1,3 @@
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+  :access_key_id     => 'AKIAJADJVBBLFOE3LPFA',
+  :secret_access_key => '/3Bt77ZlNKg3xYvXe3KgGEGH2dkdBggxb42uGXQd'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,2 @@
 # Be sure to restart your server when you modify this file.
-if ::Rails.env == 'production'
-	Web::Application.config.session_store :cookie_store, key: '_zoom_session', :domain => ".zoomcar.in"
-else
-	Web::Application.config.session_store :cookie_store, key: '_zoom_session', :domain => ".local.dev"
-end
+Web::Application.config.session_store :cookie_store, key: '_zoom_session'
