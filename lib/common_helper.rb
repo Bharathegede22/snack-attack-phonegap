325035efcc42289a5196dbc4a11ba3b1d29df520
Amit Sharma <amitkumarsharma@gmail.com>
2014-03-11 19:16:58 +0530

    min age in lib

--- lib/common_helper.rb
+++ lib/common_helper.rb
@@ -26,6 +26,7 @@ module CommonHelper
 	ALLOTMENT = 90
 	WEEKDAY_DISCOUNT = 40
 	BOOKING_WINDOW = 60
+	MIN_AGE = 21
 	
 	ENCODING_ARRAY = ["7", "c", "i", "j", "o", "k", "z", "l", "q", "r", "m", "8", "h", "u", "g", "w", "3", "1", "y", "p", "5", "s", "0", "d", "a", "e", "v", "t", "2", "4", "f", "b", "x", "6", "n", "9"]
   
