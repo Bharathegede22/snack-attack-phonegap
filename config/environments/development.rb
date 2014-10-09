6f8a6ee74a3798b6a8d9a582a6b055c43c40cb25
Amit Acharya <amitach.rajagopal@gmail.com>
2014-10-10 03:11:44 +0530

    Making pry default console for web

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,11 +16,12 @@ Web::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-	#config.action_mailer.delivery_method = :amazon_ses
+  #config.action_mailer.delivery_method = :amazon_ses
   config.action_mailer.delivery_method = :letter_opener
-  
-	config.action_mailer.default_url_options = {:host => HOSTNAME}
-	
+  config.console = Pry
+
+  config.action_mailer.default_url_options = {:host => HOSTNAME}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -31,9 +32,9 @@ Web::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
-  
+
   config.action_controller.asset_host = "http://local.dev"
-  
+
   config.cache_store = :dalli_store, {:namespace => MEMCACHED_KEY}
-  
+
 end
