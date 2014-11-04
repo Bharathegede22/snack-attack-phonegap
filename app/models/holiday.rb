4ef1078f824da0c8b1e781bc2be475e7390fe3d2
Utkarsh Mishra <utkarsh112@gmail.com>
2014-11-04 14:22:54 +0530

    pricing v4

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,11 +1,35 @@
 class Holiday < ActiveRecord::Base
+	require "#{Rails.root}/lib/datetime_helper.rb"
+	scope :blackout_holidays, -> {all}
+	class << self
+		def repeating_bod(start_date, end_date)
+			blackout_holidays.where(repeat: true).select do |holiday|
+				#TODO check laep year; see datetime_helper.rb
+				case end_date.year-start_date.year
+				when 0
+					(start_date.leap_yday..end_date.leap_yday).include?(holiday.day.leap_yday)
+				when 1
+					(start_date.leap_yday..365).include?(holiday.day.leap_yday) || (1..start_date.leap_yday).include?(holiday.day.leap_yday)
+				else
+					true
+				end
+			end
+		end
+
+		def non_repeating_bod(start_date, end_date)
+			blackout_holidays.where("holidays.repeat=? AND day<=? AND day>=?", false, end_date, start_date) 
+		end
+
+		def blackout_days(start_date, end_date)
+			(repeating_bod(start_date, end_date) | non_repeating_bod(start_date, end_date))
+		end
 	
-	def self.list
-		Rails.cache.fetch("holidays") do
-			Holiday.find_by_sql("SELECT h.name, h.day, DAY(h.day) as d, MONTH(h.day) as m FROM holidays h WHERE h.repeat = 1 OR YEAR(h.day) = #{Time.today.year} ORDER BY m ASC, d ASC")
+		def list
+			Rails.cache.fetch("holidays") do
+				Holiday.find_by_sql("SELECT h.name, h.day, DAY(h.day) as d, MONTH(h.day) as m FROM holidays h WHERE h.repeat = 1 OR YEAR(h.day) = #{Time.today.year} ORDER BY m ASC, d ASC")
+			end
 		end
 	end
-	
 end
 
 # == Schema Information
