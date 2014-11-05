1aa81105c2925543d84fbb7bdddff71f9a7bb763
Amit Kumar Sharma <amitkumarsharma@gmail.com>
2014-11-05 16:42:06 +0530

    perfomance optimization for pricing

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,9 +1,12 @@
 class Holiday < ActiveRecord::Base
+	
 	require "#{Rails.root}/lib/datetime_helper.rb"
+	
 	scope :blackout_holidays, -> {all}
+	
 	class << self
 		def repeating_bod(start_date, end_date)
-			blackout_holidays.where(repeat: true).select do |holiday|
+			blackout_holidays.where(repeat: true).to_a.select do |holiday|
 				#TODO check laep year; see datetime_helper.rb
 				case end_date.year-start_date.year
 				when 0
@@ -17,16 +20,31 @@ class Holiday < ActiveRecord::Base
 		end
 
 		def non_repeating_bod(start_date, end_date)
-			blackout_holidays.where("holidays.repeat=? AND day<=? AND day>=?", false, end_date, start_date) 
+			blackout_holidays.where("holidays.repeat=? AND day<=? AND day>=?", false, end_date, start_date).to_a
 		end
 
 		def blackout_days(start_date, end_date)
 			(repeating_bod(start_date, end_date) | non_repeating_bod(start_date, end_date))
 		end
 	
-		def list
-			Rails.cache.fetch("holidays#{Date.today}") do
-				blackout_days(Date.today, Date.today.advance(years: 1))
+		def list(year)
+			Rails.cache.fetch("holidays-#{year}") do
+				tmp = []
+				blackout_days(Date.strptime("01-01-#{year}", '%d-%m-%Y'), Date.strptime("31-12-#{year}", '%d-%m-%Y')).each do |h|
+					tmp << h.day.strftime("%m-%d")
+				end
+				tmp.sort
+			end
+		end
+
+		def active
+			year = Time.now.year
+			Rails.cache.fetch("holidays-active-#{year}") do
+				tmp = []
+				blackout_days(Date.strptime("01-01-#{year-1}", '%d-%m-%Y'), Date.strptime("31-12-#{year+1}", '%d-%m-%Y')).each do |h|
+					tmp << [h.day.strftime("%Y-%m-%d"), h]
+				end
+				tmp.sort.collect {|h| h[1]}
 			end
 		end
 	end
