92a570c9664337d6b2c4e7489c1a5157c7402d01
rajrishav0711 <rajrishav0311@gmail.com>
2014-11-24 18:34:18 +0530

    dl events

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,3 +4,20 @@ class Email < ActiveRecord::Base
 	validates :user_id, :activity, presence: true
 	
 end
+
+# == Schema Information
+#
+# Table name: emails
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  activity   :string(30)
+#  created_at :date
+#  booking_id :integer
+#
+# Indexes
+#
+#  index_emails_on_booking_id  (booking_id)
+#  index_emails_on_created_at  (created_at)
+#  index_emails_on_user_id     (user_id)
+#
