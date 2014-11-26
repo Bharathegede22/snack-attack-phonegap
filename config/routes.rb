62417fd00ddd680a0db29c0a9af5be5f4bc94ae2
tekina <aniketgarg@hotmail.com>
2014-11-26 20:21:06 +0530

    Add controller method to fetch deal details

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Web::Application.routes.draw do
-	get "deals/:id" => "main#deals"
-	get "delhi" => "main#inactive"
 	get 'mydeposits' => "wallets#show"
-  	get 'bookings'  => "wallets#show"
+  get 'bookings'  => "wallets#show"
 	get 'device'  => "main#device"
+	get 'deals/offers' => "main#deals_of_the_day"
 
 	devise_for :users, 
 		:controllers => {
