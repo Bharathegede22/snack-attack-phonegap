dc4891c1b1951c244d901bd08ead979850e06ad9
satarupa das <satarupa.das@zoomcar.com>
2014-12-26 17:56:21 +0530

    referral ui complete

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,6 +1,7 @@
 class ReportMailer < ActionMailer::Base
   
   default from: "Zoomcar <noreply@zoomcar.com>"
+  # layout 'report', only: [:referral]
   
   def exotel(error, phone, booking)
   	@error = error
@@ -14,5 +15,12 @@ class ReportMailer < ActionMailer::Base
   	@booking = @review.booking
   	mail to: "feedback@zoomcar.com", subject: "User Feedback"
   end
-  
+
+  def referral(user)
+    @user = user
+    @city = !@user.city_id.nil? ? City.find(@user.city_id) : City.find(1)
+    mail(to: @user.email, subject: "<full name of user who sent invite> wants to share the joy of self-drive with you. Sign up and earn ₹500 in credits!") do |format|
+      format.html {render layout: false}
+    end
+  end
 end
