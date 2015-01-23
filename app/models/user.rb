2ef2abbbee9b795bcfafe3092e52aedef5434071
Rohit <zoomcar@Zoomcars-MacBook-Air-6.local>
2015-01-23 12:08:58 +0530

    dob validation skip

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ActiveRecord::Base
 
 	def generate_referral_code
 		self.ref_code = Base64.urlsafe_encode64(self.id.to_s)
-		self.save!
+		self.save(validate: false)
 	end
 	
 	def get_bookings(action, page=0)
