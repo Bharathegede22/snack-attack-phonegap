31c052f80e3fc300691013122fbbcd5df61fbfc6
natasha1403 <natashaagarwal1403@gmail.com>
2015-02-06 14:44:20 +0530

    trip shorten discount calc fix

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -103,7 +103,7 @@ module BookingsHelper
 
   def update_reschedule_params(params, booking)
     params[:promo] = booking.promo
-    params[:id] = booking.id
+    params[:booking_id] = booking.confirmation_key
     params[:city_id] = @city.id
     params[:auth_token] = @current_user.authentication_token
     params[:starts] = Time.zone.parse(params[:starts]) if params[:starts].present?
