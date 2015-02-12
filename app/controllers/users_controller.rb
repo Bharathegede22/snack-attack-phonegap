c9f14e5d806179915aecc2a4c8c7a462ca5987c7
bhuvnesh singh <bhuvnesh@zoomcar.in>
2015-02-12 15:07:50 +0530

    license upload changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,9 @@ class UsersController < ApplicationController
 				@hash = {}
 				image_arr = []
 				image_count = 0
-				if params[:image_id].present? && !current_user.license_pic.blank?
-					img = Image.where("imageable_id = ? AND id = ? AND imageable_type = 'License'", current_user.id,params[:image_id].to_i)
-					all_pics = Image.where("imageable_id = ? AND imageable_type = 'License'",current_user.id)
+				if params[:image_id].present? && !@admin_user.license_pic.blank?
+					img = Image.where("imageable_id = ? AND id = ? AND imageable_type = 'License'", @admin_user.id,params[:image_id].to_i)
+					all_pics = Image.where("imageable_id = ? AND imageable_type = 'License'",@admin_user.id)
 					image_count = all_pics.count if !all_pics.blank?
 					if img.present?
 						img.first.avatar.destroy
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 							# image_arr << {delete_status: 1, count: image_count}
 							# last_count = Image.where("imageable_id = ? AND imageable_type = 'License'",current_user.id)
 							if image_count == 0
-								User.find(current_user.id).update_column(:license_status,0)
+								User.find(@admin_user.id).update_column(:license_status,0)
 								image_arr << {delete_status: 1, count: image_count,final_status: 0}
 							else
 								image_arr << {delete_status: 1, count: image_count,final_status: 1}
@@ -51,13 +51,13 @@ class UsersController < ApplicationController
     	@image_arr = []
     	image_count = 0
     	count = 0
-    	image = Image.where("imageable_id = ? AND imageable_type = 'License'",current_user.id)
+    	image = Image.where("imageable_id = ? AND imageable_type = 'License'",@admin_user.id)
     	if image.blank?
     		image_count = 0 
     	else
     		image_count = image.count
     	end
-	    @image_arr << {count: image_count,status: current_user.license_status}
+	    @image_arr << {count: image_count,status: @admin_user.license_status}
 	    if !image.blank?
 	    	image.each do|img|
 	    	# urll = "http://local.dev"
@@ -72,30 +72,39 @@ class UsersController < ApplicationController
 	end
 
 	def license
+		if params[:license_approval_id].present? && current_user.role >= 6
+			@admin_user = User.where(id: params[:license_approval_id].to_i).first
+			if @admin_user.nil?
+				flash[:error] = 'select correct user'
+				return
+			end
+		else
+			@admin_user = current_user@admin_user
+		end
     if request.post? 
 	      @hash = {}
 	      @image_arr = []
 	      count = 0
-		   	image_count = Image.where("imageable_id = ? AND imageable_type = 'License'",current_user.id).count
+		   	image_count = Image.where("imageable_id = ? AND imageable_type = 'License'",@admin_user.id).count
 		   	if image_count < 5 
 			   	@image = Image.new(image_params)
-					@image.imageable_id = current_user.id
+					@image.imageable_id = @admin_user.id
 					@image.imageable_type = 'License'
 					if @image.save
 						image_count = (image_count + 1)
-						current_user.license_status = 1
-						current_user.save(:validate=>false)
-				    @image_arr << {count: image_count,status: current_user.license_status}
+						@admin_user.license_status = 1
+						@admin_user.save(:validate=>false)
+				    @image_arr << {count: image_count,status: @admin_user.license_status}
 		    		# urll = "http://local.dev"
 		   	  	urll = @image.avatar.url
 		   	  	@image_arr << {url: urll, image_id: @image.id}
 		   	  else
-		   	  	@image_arr << {count: image_count,status: current_user.license_status,error: 1}
+		   	  	@image_arr << {count: image_count,status: @admin_user.license_status,error: 1}
 				  end
 			      @hash = {image: @image_arr}
 			      render :json => @hash
 			 	else
-			 			@image_arr << {count: image_count,status: current_user.license_status,error: 0}
+			 			@image_arr << {count: image_count,status: @admin_user.license_status,error: 0}
 			 			
 			    	####
 			  end
